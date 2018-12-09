1c2b357b238a139c7d87115c6b90b3e4201a96d4
Maria <murikmarusia@gmail.com>
2018-12-10 00:03:11 +0200

    Added js

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,71 +1,20 @@
-var form = document.querySelector(".form-container");
-var btn = document.querySelector(".btn");
-var fields = document.querySelectorAll(".field");
+(function anon() {
+  const space = document.querySelector(".container");
 
-var textVal = document.querySelectorAll(".textVal");
-var emailVal = document.querySelector(".emailVal");
-var passwordVal = document.querySelector(".passwordVal");
+  const createTooltip = function(x, y) {
+    const newEl = document.createElement("div");
+    newEl.classList.add("tooltip");
+    newEl.textContent = "x:" + x + ", " + "y:" + y;
+    space.appendChild(newEl);
+    newEl.style.left = x - 90 + "px";
+    newEl.style.top = y - 60 + "px";
+  };
 
-
-form.addEventListener("submit", function(event) {
-  event.preventDefault();
-  fields.forEach(element => {
-    element.classList.remove("empty");
-    if (element.value == 0) {
-      element.classList.add("empty");
-    } 
+  space.addEventListener("mousemove", event => {
+    document.querySelector(".tooltip").remove();
+    event = event || window.event;
+    let x = event.pageX;
+    let y = event.pageY;
+    createTooltip(x, y);
   });
-  var emptyFieldsCounter = document.querySelectorAll(".empty").length;
-  if (emptyFieldsCounter == 0) {
-    form.submit();
-  }
-});
-
-textVal.forEach(element => {
-  element.addEventListener("input", function(event) {
-    deleteErrors();
-    var abs = element.value;
-    this.style.border = "1px solid green";
-
-    if(abs.match(/['"]/)) {
-      addError(element, "This field can not include &#39 and &#34");
-    }
-  });
-});
-
-emailVal.addEventListener("input", function(event) {
-  deleteErrors();
-  var abs = this.value;
-  this.style.border = "1px solid green";
-
-  if(!(abs.match(/[0-9a-z_]+@[0-9a-z_]+\.[a-z]{2,5}/i))) {
-    addError(emailVal, "This field should be like asd@asd.yu");
-  }
-});
-
-passwordVal.addEventListener("input", function(event) {
-  deleteErrors();
-  var abs = this.value;
-  this.style.border = "1px solid green";
-
-  if(!(abs.match(/[0-9a-z_]{6}/i))) {
-    addError(passwordVal, "At least six characters");
-  }
-});
-
-function addError(element, errorMessage) {
-  var error = document.createElement("div");
-  error.className = "error";
-  error.style.color = "red";
-  error.innerHTML = errorMessage;
-  element.parentElement.appendChild(error);
-  element.style.border = "1px solid red";
-}
-function deleteErrors() {
-  var errors = form.querySelectorAll(".error");
-  for(var i = 0; i < errors.length; i++) {
-    errors[i].remove();
-  }
-}
-
-
+})();
