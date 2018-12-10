b434fb979fa4e961ca77116000bd3664c319372c
Maria <murikmarusia@gmail.com>
2018-12-10 17:49:08 +0200

    Added data-atribute

--- src/js/main.js
+++ src/js/main.js
@@ -1,8 +1,12 @@
 (function anon() {
   const space = document.querySelector('.container');
-  const tooltip = document.querySelector('.tooltip');
+  const container = document.querySelector('[data-attr="tooltip"]');
+  
+  const tooltip = document.createElement('div');
+  tooltip.classList.add('tooltip');
+  container.appendChild(tooltip);
 
-  const addCoordinates = (x, y) => {
+  const addTooltip = (x, y) => {
     tooltip.textContent = `x:${x}, ` + `y:${y}`;
     tooltip.style.left = `${x - 90}px`;
     tooltip.style.top = `${y - 80 }px`;
@@ -10,13 +14,15 @@
 
   window.addEventListener('mousemove', (event) => {
     event = event || window.event;
+
     if (event.target.parentNode === space) {
       event = event || window.event;
       const x = event.pageX;
       const y = event.pageY;
-      addCoordinates(x, y);
+      addTooltip(x, y);
+      
     } else {
-      addCoordinates(-100, -100);
+      addTooltip(-100, -100);
     }
   });
 }());
