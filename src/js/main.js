98dfe79f0c61a9029ac53a508c3e10a06b0f47b7
Maria <murikmarusia@gmail.com>
2019-01-10 10:10:50 +0200

    added setTimeout function

--- src/js/main.js
+++ src/js/main.js
@@ -1,28 +1,59 @@
-(function anon() {
-  const space = document.querySelector('.container');
-  const container = document.querySelector('[data-attr="tooltip"]');
-  
-  const tooltip = document.createElement('div');
-  tooltip.classList.add('tooltip');
-  container.appendChild(tooltip);
+const space = document.querySelector('.container');
+const container = document.querySelector('[data-attr="tooltip"]');
 
-  const addTooltip = (x, y) => {
-    tooltip.textContent = `x:${x}, ` + `y:${y}`;
-    tooltip.style.left = `${x - 90}px`;
-    tooltip.style.top = `${y - 80 }px`;
-  };
+const tooltip = document.createElement('div');
+tooltip.classList.add('tooltip');
+container.appendChild(tooltip);
 
-  window.addEventListener('mousemove', (event) => {
-    event = event || window.event;
+function throttle(func, ms) {
+  let isThrottled = false;
+  let savedArgs = null;
+  let savedThis = null;
 
-    if (event.target.parentNode === space) {
-      event = event || window.event;
-      const x = event.pageX;
-      const y = event.pageY;
-      addTooltip(x, y);
-      
-    } else {
-      addTooltip(-100, -100);
+  function wrapper() {
+    if (isThrottled) {
+      savedArgs = arguments;
+      savedThis = this;
+      return;
     }
-  });
-}());
+
+    func.apply(this, arguments);
+
+    isThrottled = true;
+
+    setTimeout(() => {
+      isThrottled = false;
+      if (savedArgs) {
+        wrapper.apply(savedThis, savedArgs);
+        savedArgs = savedThis = null;
+      }
+    }, ms);
+  }
+  return wrapper;
+}
+
+const positioningTooltip = (x, y) => {
+  tooltip.style.left = `${x - 90}px`;
+  tooltip.style.top = `${y - 80}px`;
+};
+
+const addContent = (x, y) => {
+  tooltip.textContent = `x:${x}, ` + `y:${y}`;
+};
+
+const f = throttle(addContent, 1000);
+
+window.addEventListener('mousemove', (event) => {
+  event = event || window.event;
+
+  if (event.target.parentNode === space) {
+    const x = event.pageX;
+    const y = event.pageY;
+    positioningTooltip(x, y);
+    f(x, y);
+  } else {
+    positioningTooltip(-100, -100);
+  }
+});
+
+
